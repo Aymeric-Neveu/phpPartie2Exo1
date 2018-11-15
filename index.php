049cf56c8a01c6bd12f04a34c05c88513a66c7e0
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 14:16:21 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $age = "16";
+    echo "$age";
+    if ($age <= 17) {
+      echo " Vous êtes mineur";
+    }
+    else {
+      echo " Vous êtes majeur";
+    }
+    ?>
+  </body>
